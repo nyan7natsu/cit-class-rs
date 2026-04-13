26311870aeeb3a7de055932b1b9b1f444c14f938
nyan7natsu <166627121+nyan7natsu@users.noreply.github.com>
2026-04-13 15:58:13 +0900

    fix types

--- src/types.rs
+++ src/types.rs
@@ -3,48 +3,48 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct StatusDto {
     pub success: bool,
-    #[serde(alias = "messageList")]
+    #[serde(rename = "messageList")]
     pub message_list: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct LoginResponseData {
-    #[serde(alias = "userId")]
+    #[serde(rename = "userId")]
     pub user_id: String,
-    #[serde(alias = "encryptedPassword")]
+    #[serde(rename = "encryptedPassword")]
     pub encrypted_password: String,
-    #[serde(alias = "userName")]
+    #[serde(rename = "userName")]
     pub user_name: String,
-    #[serde(alias = "userShkbtKbn")]
+    #[serde(rename = "userShkbtKbn")]
     pub user_shkbt_kbn: String,
-    #[serde(alias = "shokuinUserKbn")]
+    #[serde(rename = "shokuinUserKbn")]
     pub shokuin_user_kbn: Option<serde_json::Value>,
-    #[serde(alias = "jinjiCd")]
+    #[serde(rename = "jinjiCd")]
     pub jinji_cd: Option<serde_json::Value>,
-    #[serde(alias = "kanriNo")]
+    #[serde(rename = "kanriNo")]
     pub kanri_no: i32,
-    #[serde(alias = "gaksekiCd")]
+    #[serde(rename = "gaksekiCd")]
     pub gakseki_cd: String,
     pub name: String,
-    #[serde(alias = "nameKana")]
+    #[serde(rename = "nameKana")]
     pub name_kana: String,
-    #[serde(alias = "nameEng")]
+    #[serde(rename = "nameEng")]
     pub name_eng: String,
-    #[serde(alias = "nameDisp")]
+    #[serde(rename = "nameDisp")]
     pub name_disp: String,
-    #[serde(alias = "validKikanStartDatetime")]
+    #[serde(rename = "validKikanStartDatetime")]
     pub valid_kikan_start_datetime: Option<serde_json::Value>,
-    #[serde(alias = "validKikanEndDatetime")]
+    #[serde(rename = "validKikanEndDatetime")]
     pub valid_kikan_end_datetime: Option<serde_json::Value>,
-    #[serde(alias = "menuPtnCd")]
+    #[serde(rename = "menuPtnCd")]
     pub menu_ptn_cd: Option<serde_json::Value>,
-    #[serde(alias = "langCd")]
+    #[serde(rename = "langCd")]
     pub lang_cd: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct LoginResponse {
-    #[serde(alias = "statusDto")]
+    #[serde(rename = "statusDto")]
     pub status_dto: StatusDto,
     pub data: Option<LoginResponseData>,
 }
@@ -55,16 +55,16 @@ pub struct ClassData {
     pub nendo: i32,
 
     /** 授業コード */
-    #[serde(alias = "jugyoCd")]
+    #[serde(rename = "jugyoCd")]
     pub jugyo_cd: String,
 
     /** 開講年度 */
-    #[serde(alias = "kaikoNendo")]
+    #[serde(rename = "kaikoNendo")]
     pub kaiko_nendo: i32,
 
     /** 開講学期 */
-    #[serde(alias = "gakkiNo")]
-    pub gakki_no: i32,
+    #[serde(rename = "gakkiNo")]
+    pub gakki_no: i8,
 
     /**
      * 授業区分
@@ -72,50 +72,70 @@ pub struct ClassData {
      * `"1"`: 通常授業  
      * `"6"`: 集中授業
      */
-    #[serde(alias = "jugyoKbn")]
+    #[serde(rename = "jugyoKbn")]
     pub jugyo_kbn: String,
 
+    /** 授業名 */
+    #[serde(rename = "jugyoName")]
+    pub jugyo_name: String,
+
+    /** 開講曜日 */
+    #[serde(rename = "kaikoYobi")]
+    pub kaiko_yobi: i8,
+
+    /** 開講時限 */
+    #[serde(rename = "jigenNo")]
+    pub jigen_no: i8,
+
     /**
-     * 授業終了時間
+     * 授業開始時間 (HHMMの形式)
      *
      * 集中講義の場合は `null` になる
      */
-    #[serde(alias = "jugyoEndTime")]
+    #[serde(rename = "jugyoStartTime")]
+    pub jugyo_start_time: Option<String>,
+
+    /**
+     * 授業終了時間 (HHMMの形式)
+     *
+     * 集中講義の場合は `null` になる
+     */
+    #[serde(rename = "jugyoEndTime")]
     pub jugyo_end_time: Option<String>,
 
     /** 指導教員名 */
-    #[serde(alias = "kyoinName")]
+    #[serde(rename = "kyoinName")]
     pub kyoin_name: String,
 
     /** 教室名 */
-    #[serde(alias = "kyostName")]
+    #[serde(rename = "kyostName")]
     pub kyost_name: Option<String>,
 
     /** 掲示の未読数 */
-    #[serde(alias = "keijiMidokCnt")]
+    #[serde(rename = "keijiMidokCnt")]
     pub keiji_midok_cnt: i32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ClassSchedule {
     pub nendo: i32,
-    #[serde(alias = "gakkiNo")]
+    #[serde(rename = "gakkiNo")]
     pub gakki_no: i32,
-    #[serde(alias = "gakkiName")]
+    #[serde(rename = "gakkiName")]
     pub gakki_name: String,
-    #[serde(alias = "jgkmDtoList")]
+    #[serde(rename = "jgkmDtoList")]
     pub jgkm_dto_list: Vec<ClassData>,
-    #[serde(alias = "keijiCnt")]
+    #[serde(rename = "keijiCnt")]
     pub keiji_cnt: i32,
-    #[serde(alias = "funcIdList")]
+    #[serde(rename = "funcIdList")]
     pub func_id_list: Vec<String>,
-    #[serde(alias = "maxGakkiNo")]
+    #[serde(rename = "maxGakkiNo")]
     pub max_gakki_no: i32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ClassScheduleResponse {
-    #[serde(alias = "statusDto")]
+    #[serde(rename = "statusDto")]
     pub status_dto: StatusDto,
     pub data: Option<ClassSchedule>,
 }
